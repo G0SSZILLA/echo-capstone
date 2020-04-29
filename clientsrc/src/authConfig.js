a5b74d799eaf856063ad4a8eaf24bbfd40fa815a
MatthewJOwens <mjowens@mail.com>
2020-04-29 13:59:23 -0600

    new Identifier

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "betasaur-echo.auth0.com";
-export const audience = "https://betasaur-echo.auth0.com/api/v2/";
+export const audience = "https://betasaur-echo.com";
 export const clientId = "RAYctHhDjt24LSyp2C78MijFQI1dqDyQ";
