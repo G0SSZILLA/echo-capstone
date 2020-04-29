220d28fd33f5c22f0fc0f606f9d1478bc68fbab2
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-29 14:09:55 -0600

    username => name

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -6,7 +6,7 @@ const Profile = new Schema(
   {
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
-    username: { type: String, required: true },
+    name: { type: String, required: true },
     picture: { type: String },
     // TODO user role setup, learn w/ team 
   },
