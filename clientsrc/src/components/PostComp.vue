a769b43d9e73635350503b2161901ef43394a955
Nate Sherfick <natesherfick@gmail.com>
2020-05-01 15:53:51 -0600

    Results bar working

--- clientsrc/src/components/PostComp.vue
+++ clientsrc/src/components/PostComp.vue
@@ -2,10 +2,10 @@
   <div class="postComp">
     <div class="card m-2">
       <img class="card-img-top" v-if="postData.picture" :src="postData.picture" />
-      <div class="card-body">
+      <div class="card-body pb-2">
         <h5 class="card-title">{{postData.title}}</h5>
         <p v-show="displayContent" class="card-text">{{postData.content}}</p>
-        <div class="card-footer" v-if="$auth.isAuthenticated">
+        <div class="card-footer px-2 pb-0 pt-3 bg-white" v-if="$auth.isAuthenticated">
           <div v-if="$auth.user.email_verified">
             <a
               href="#"
@@ -19,8 +19,8 @@
               v-show="showButton"
               @click.prevent="chooseDisregard()"
             >disregard</a>
-            <!-- <Results v-show="!showButton" :postData="postData" /> -->
-            <p @click="goToDetails()" v-show="!showButton">Join the Conversation</p>
+            <ResultsComp v-if="!showButton" :postData="postData" />
+            <p class='mb-0 pb-0' @click="goToDetails()" v-show="!showButton">Join the Conversation</p>
           </div>
           <small v-else>User email is not verified.</small>
         </div>
@@ -35,6 +35,7 @@
 
 <script>
 import { getInstance } from "@bcwdev/auth0-vue";
+import ResultsComp from '../components/ResultsComp.vue'
 export default {
   name: "postComp",
   props: ["postData"],
@@ -62,6 +63,7 @@ export default {
       this.showButton = false;
       this.postData.support.push(this.$auth.user.email);
       this.$store.dispatch("addUserInput", this.postData);
+
     },
 
     chooseDisregard() {
@@ -102,7 +104,9 @@ export default {
     this.$router.push("posts/" + this.postData.id);
   },
 
-  components: {}
+  components: {
+      ResultsComp,
+  }
 };
 </script>
 
