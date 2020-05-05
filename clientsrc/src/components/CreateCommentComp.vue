9875ec37700da5ece9908c821aaed725f41f84f7
MatthewJOwens <mjowens@mail.com>
2020-05-05 09:36:30 -0600

    routed Back button on Post Details page

--- clientsrc/src/components/CreateCommentComp.vue
+++ clientsrc/src/components/CreateCommentComp.vue
@@ -1,58 +1,61 @@
 <template>
-    <div class="createCommentComp form">
-<footer class="page-footer bg-info fixed-bottom">
-    <div class="row p-1">
-<div class="col-3">
-    <button class="btn border bg-warning mt-2 ml-1">
-        Back
-    </button>
-</div>
-<div class="col-6 p-0">
-  <div class="md-form pink-textarea active-pink-textarea-2">
-  <textarea id="form17" class="md-textarea form-control" v-model="newComment.content" placeholder="Comment here..." rows="2"></textarea>
-</div>
-
-</div>
-<div class="col-3" >
-    <button type="submit" @click="addComment" class="btn border bg-warning mt-2 mr-1">
-        Send
-    </button>
-</div>
-
-    </div>
-</footer>
-    </div>
+  <div class="createCommentComp form">
+    <footer class="page-footer bg-info fixed-bottom">
+      <div class="row p-1">
+        <div class="col-3">
+          <button class="btn border bg-warning mt-2 ml-1" @click="backToHome()">Back</button>
+        </div>
+        <div class="col-6 p-0">
+          <div class="md-form pink-textarea active-pink-textarea-2">
+            <textarea
+              id="form17"
+              class="md-textarea form-control"
+              v-model="newComment.content"
+              placeholder="Comment here..."
+              rows="2"
+            ></textarea>
+          </div>
+        </div>
+        <div class="col-3">
+          <button type="submit" @click="addComment" class="btn border bg-warning mt-2 mr-1">Send</button>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 
 <script>
 export default {
-    name: 'createCommentComp',
-    data(){
-        return {
-          newComment: {}
-        }
-    },
-    computed:{
-      activePost(){
-        return this.$store.state.activePost
+  name: "createCommentComp",
+  data() {
+    return {
+      newComment: {}
+    };
+  },
+  computed: {
+    activePost() {
+      return this.$store.state.activePost;
+    }
+  },
+  methods: {
+    addComment() {
+      if (this.activePost.support.find(i => i == this.$auth.userInfo.email)) {
+        this.newComment.support = true;
+      } else {
+        this.newComment.support = false;
       }
+
+      this.newComment.postId = this.activePost.id;
+      this.$store.dispatch("addComment", this.newComment);
+      this.newComment = {};
     },
-    methods:{
-      addComment() {
-        if(this.activePost.support.find(i => i == this.$auth.userInfo.email)){
-          this.newComment.support = true
-        }else{
-          this.newComment.support = false
-        }
-        
-        this.newComment.postId = this.activePost.id
-        this.$store.dispatch('addComment', this.newComment)
-        this.newComment = {}
-      },
-    },
-    components:{}
-}
+    backToHome() {
+      this.$router.push({ name: "home" });
+    }
+  },
+  components: {}
+};
 </script>
 
 
@@ -67,8 +70,9 @@ export default {
 .active-pink-textarea-2.md-form label {
   color: #f48fb1;
 }
-.active-pink-textarea-2.md-form textarea.md-textarea:focus:not([readonly])+label {
+.active-pink-textarea-2.md-form
+  textarea.md-textarea:focus:not([readonly])
+  + label {
   color: #f48fb1;
 }
-
 </style>
