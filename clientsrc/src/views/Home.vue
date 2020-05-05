670798deef29bf3c5e3174afc09ebb1f92b4e5d8
MatthewJOwens <mjowens@mail.com>
2020-05-05 10:55:02 -0600

    Sort by newest implemented

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -4,32 +4,38 @@
       <div class="col-3"></div>
       <div class="col-6"></div>
       <div class="col-3">
-   <CreatePostComp v-if="$auth.isAuthenticated" />
+        <CreatePostComp v-if="$auth.isAuthenticated" />
       </div>
-      </div>
-   <PostComp v-for='post in posts' :key='post.id' :postData='post'/>
+    </div>
+    <PostComp v-for="post in posts" :key="post.id" :postData="post" />
   </div>
 </template>
 
 
 <script>
-import PostComp from '../components/PostComp.vue'
-import CreatePostComp from '../components/CreatePostComp.vue'
+import PostComp from "../components/PostComp.vue";
+import CreatePostComp from "../components/CreatePostComp.vue";
 export default {
   name: "home",
   data() {
-    return {};
+    return {
+      newest: true
+    };
   },
   created() {
-    this.$store.dispatch('getPosts')
+    this.$store.dispatch("getPosts");
   },
   computed: {
     posts() {
-      return this.$store.state.posts
+      if (this.newest) {
+        return this.$store.state.posts.sort(
+          (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
+        );
+      }
     }
   },
   methods: {},
-  components: {CreatePostComp, PostComp}
+  components: { CreatePostComp, PostComp }
 };
 </script>
 
