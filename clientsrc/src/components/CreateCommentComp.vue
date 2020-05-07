e29aecab5a09e90b8ee5acf84858062d09834c45
G0SSZILLA <gossman15@gmail.com>
2020-05-07 14:07:35 -0600

    colors

--- clientsrc/src/components/CreateCommentComp.vue
+++ clientsrc/src/components/CreateCommentComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="createCommentComp form">
-    <footer class="page-footer bg-info fixed-bottom">
+    <footer class="page-footer bg-dark fixed-bottom">
       <div class="row p-1">
         <div class="col-3">
           <button class="btn border bg-warning mt-2 ml-1" @click="backToHome()">Back</button>
@@ -61,19 +61,12 @@ export default {
 
 
 <style scoped>
-.active-pink-textarea-2 textarea.md-textarea {
-  border-bottom: 1px solid #f48fb1;
-  box-shadow: 0 1px 0 0 #f48fb1;
+
+textarea{
+  outline: none;
+  resize: none;
 }
-.active-pink-textarea-2.md-form label.active {
-  color: #f48fb1;
-}
-.active-pink-textarea-2.md-form label {
-  color: #f48fb1;
-}
-.active-pink-textarea-2.md-form
-  textarea.md-textarea:focus:not([readonly])
-  + label {
-  color: #f48fb1;
-}
+
+
+
 </style>
