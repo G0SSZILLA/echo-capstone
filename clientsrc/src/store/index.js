1047a72ccd92f8d1cdacdc033c0456a649be920f
MatthewJOwens <mjowens@mail.com>
2020-05-07 15:36:46 -0600

    wrote a lastLoaded logic

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -8,172 +8,180 @@ Vue.use(Vuex);
 
 //Allows axios to work locally or live
 export const base = window.location.host.includes("localhost") ?
-    "//localhost:3000/" :
-    "/";
+  "//localhost:3000/" :
+  "/";
 
 let api = Axios.create({
-    baseURL: base + "api/",
-    timeout: 10000,
-    withCredentials: true,
+  baseURL: base + "api/",
+  timeout: 10000,
+  withCredentials: true,
 });
 
 export default new Vuex.Store({
-    state: {
-        user: {},
-        post: {},
-        activePost: { support: [], disregard: [] },
-        posts: [],
-        comments: [],
-        choice: {},
-        loadingPosts: false
+  state: {
+    user: {},
+    post: {},
+    activePost: { support: [], disregard: [] },
+    posts: [],
+    comments: [],
+    choice: {},
+    loadingPosts: false,
+    lastLoaded: false
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
     },
-    mutations: {
-        setUser(state, user) {
-            state.user = user;
-        },
-        setPost(state, post) {
-            state.post = post;
-        },
-        setPosts(state, posts) {
-            state.posts.push(...posts);
-        },
-        setActivePost(state, post = { support: [], disregard: [] }) {
-            state.activePost = post;
-        },
-        setComments(state, comments) {
-            state.comments = comments;
-        },
-        setLoadingPosts(state, bool) {
-            state.loadingPosts = bool
+    setPost(state, post) {
+      state.post = post;
+    },
+    setPosts(state, posts) {
+      state.posts.push(...posts);
+    },
+    setActivePost(state, post = { support: [], disregard: [] }) {
+      state.activePost = post;
+    },
+    setComments(state, comments) {
+      state.comments = comments;
+    },
+    setLoadingPosts(state, bool) {
+      state.loadingPosts = bool
+    },
+    setLastLoaded(state, bool) {
+      state.lastLoaded = bool
+    }
+
+  },
+  actions: {
+    //#region -- AUTH STUFF --
+    setBearer({ dispatch }, bearer) {
+      api.defaults.headers.authorization = bearer;
+      dispatch('initializeSocket', bearer);
+    },
+    resetBearer() {
+      api.defaults.headers.authorization = "";
+    },
+    async getProfile({ commit }) {
+      try {
+        let res = await api.get("profile");
+        commit("setUser", res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    async addUserInput({ commit, dispatch }, postData) {
+      try {
+        let res = await api.put("posts/" + postData.id + "/vote", postData);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    //#endregion
+
+    //#region -- POSTS --
+
+    async getPosts({ commit, dispatch }, postsLength) {
+      return new Promise(async (resolve, reject) => {
+        try {
+
+          let res = await api.get("posts?skip=" + postsLength);
+          console.log("from get posts in store", res);
+          commit("setPosts", res.data);
+          resolve()
+          commit("setLoadingPosts", false)
+          //NOTE change this if .limit is changed in PostsService
+          if (res.data.length < 5) {
+            commit("setLastLoaded", true)
+          }
+        } catch (error) {
+          console.error(error, "failed to get posts from get posts in store");
+          reject(error)
+        }
+      })
+    },
+
+    async getPost({ commit, dispatch }, postId) {
+      try {
+        let res = await api.get(`posts/${postId}`);
+        if (res.data.id) {
+          commit("setActivePost", res.data);
         }
 
+        if (
+          res.data.support &&
+          res.data.support.find(i => i == this.$app.$auth.user.email)
+        ) {
+          dispatch(
+            "JoinRoom",
+            res.data.id + ":support"
+          );
+        } else {
+          dispatch(
+            "JoinRoom",
+            res.data.id + ":disregard"
+          );
+
+        };
+
+      } catch (error) {
+        console.error(error);
+      }
     },
-    actions: {
-        //#region -- AUTH STUFF --
-        setBearer({ dispatch }, bearer) {
-            api.defaults.headers.authorization = bearer;
-            dispatch('initializeSocket', bearer);
-        },
-        resetBearer() {
-            api.defaults.headers.authorization = "";
-        },
-        async getProfile({ commit }) {
-            try {
-                let res = await api.get("profile");
-                commit("setUser", res.data);
-            } catch (err) {
-                console.error(err);
-            }
-        },
 
-        async addUserInput({ commit, dispatch }, postData) {
-            try {
-                let res = await api.put("posts/" + postData.id + "/vote", postData);
-            } catch (err) {
-                console.error(err);
-            }
-        },
-        //#endregion
+    async addPost({ commit, dispatch }, postData) {
+      try {
+        let res = await api.post("posts", postData);
+        console.log("addPost from store", postData);
+        // NOTE do we want this to push us to the post details page
+        // dispatch("getPosts");
 
-        //#region -- POSTS --
-
-        async getPosts({ commit, dispatch }, postsLength) {
-            return new Promise(async(resolve, reject) => {
-                try {
-
-                    let res = await api.get("posts?skip=" + postsLength);
-                    console.log("from get posts in store", res);
-                    commit("setPosts", res.data);
-                    resolve()
-                    commit("setLoadingPosts", false)
-                } catch (error) {
-                    console.error(error, "failed to get posts from get posts in store");
-                    reject(error)
-                }
-            })
-        },
-
-        async getPost({ commit, dispatch }, postId) {
-            try {
-                let res = await api.get(`posts/${postId}`);
-                if (res.data.id) {
-                    commit("setActivePost", res.data);
-                }
-
-                if (
-                    res.data.support &&
-                    res.data.support.find(i => i == this.$app.$auth.user.email)
-                ) {
-                    dispatch(
-                        "JoinRoom",
-                        res.data.id + ":support"
-                    );
-                } else {
-                    dispatch(
-                        "JoinRoom",
-                        res.data.id + ":disregard"
-                    );
-
-                };
-
-            } catch (error) {
-                console.error(error);
-            }
-        },
-
-        async addPost({ commit, dispatch }, postData) {
-            try {
-                let res = await api.post("posts", postData);
-                console.log("addPost from store", postData);
-                // NOTE do we want this to push us to the post details page
-                // dispatch("getPosts");
-
-            } catch (error) {
-                console.error(error, "addPost in store failing");
-            }
-        },
-        async deletePost({ dispatch }, postId) {
-            try {
-                await api.delete("posts/" + postId)
-                dispatch("getPosts")
-            } catch (error) {
-                console.error("deletePost failed: ", error);
-            }
-        },
-        //#endregion
-
-        //#region -- COMMENTS  --
-        async getComments({ commit }, postId) {
-            try {
-                let res = await api.get("posts/" + postId + "/comments");
-                commit("setComments", res.data);
-            } catch (error) {
-                console.error("getComments failing", error);
-            }
-        },
-        async addComment({ dispatch }, comment) {
-            try {
-                let res = await api.post("comments/", comment);
-                console.log("addComment: ", res.data);
-                // dispatch("getComments", comment.postId);
-            } catch (error) {
-                console.error("addComment failing: ", error);
-            }
-        },
-        async deleteComment({ dispatch }, comment) {
-            try {
-                await api.delete("comments/" + comment.id)
-                dispatch("getComments", comment.postId)
-            } catch (error) {
-                console.error("deleteComment failed: ", error);
-            }
-        },
-        //#endregion
-
-        //#region -- WHATEVS 4 NOW--
-        //#endregion
+      } catch (error) {
+        console.error(error, "addPost in store failing");
+      }
     },
-    modules: {
-        SocketStore
-    }
+    async deletePost({ dispatch }, postId) {
+      try {
+        await api.delete("posts/" + postId)
+        dispatch("getPosts")
+      } catch (error) {
+        console.error("deletePost failed: ", error);
+      }
+    },
+    //#endregion
+
+    //#region -- COMMENTS  --
+    async getComments({ commit }, postId) {
+      try {
+        let res = await api.get("posts/" + postId + "/comments");
+        commit("setComments", res.data);
+      } catch (error) {
+        console.error("getComments failing", error);
+      }
+    },
+    async addComment({ dispatch }, comment) {
+      try {
+        let res = await api.post("comments/", comment);
+        console.log("addComment: ", res.data);
+        // dispatch("getComments", comment.postId);
+      } catch (error) {
+        console.error("addComment failing: ", error);
+      }
+    },
+    async deleteComment({ dispatch }, comment) {
+      try {
+        await api.delete("comments/" + comment.id)
+        dispatch("getComments", comment.postId)
+      } catch (error) {
+        console.error("deleteComment failed: ", error);
+      }
+    },
+    //#endregion
+
+    //#region -- WHATEVS 4 NOW--
+    //#endregion
+  },
+  modules: {
+    SocketStore
+  }
 });
