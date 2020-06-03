9ee19cf7f774b1f8727c80165c771a7b3387cf3d
MatthewJOwens <mjowens@mail.com>
2020-06-03 15:41:22 -0600

    wrong case on file import

--- server/controllers/CommentsController.js
+++ server/controllers/CommentsController.js
@@ -2,59 +2,59 @@ import express from 'express'
 import BaseController from "../utils/BaseController";
 import auth0provider from "@bcwdev/auth0provider";
 import { commentsService } from '../services/CommentsService'
-import CheckEmail from "../utils/CheckEmail";
+import CheckEmail from "../utils/checkEmail";
 
 
 
 //PUBLIC
 export class CommentsController extends BaseController {
-    constructor() {
-        super("api/comments")
-        this.router
-            .use(auth0provider.getAuthorizedUserInfo)
-            .get('', this.getAll)
-            .get('/:id', this.getById)
-            .use(CheckEmail)
-            .post('', this.create)
-            .put('/:id', this.edit)
-            .delete('/:id', this.delete)
-    }
+  constructor() {
+    super("api/comments")
+    this.router
+      .use(auth0provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .use(CheckEmail)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
 
 
-    async getAll(req, res, next) {
-        try {
-            //only gets comments by user who is logged in
-            let data = await commentsService.getAll()
-            return res.send(data)
-        } catch (err) { next(err) }
-    }
+  async getAll(req, res, next) {
+    try {
+      //only gets comments by user who is logged in
+      let data = await commentsService.getAll()
+      return res.send(data)
+    } catch (err) { next(err) }
+  }
 
-    async getById(req, res, next) {
-        try {
-            let data = await commentsService.getById(req.params.id, req.userInfo.email)
-            return res.send(data)
-        } catch (error) { next(error) }
-    }
+  async getById(req, res, next) {
+    try {
+      let data = await commentsService.getById(req.params.id, req.userInfo.email)
+      return res.send(data)
+    } catch (error) { next(error) }
+  }
 
-    async create(req, res, next) {
-        try {
-            req.body.creatorEmail = req.userInfo.email
-            let data = await commentsService.create(req.body)
-            return res.status(201).send(data)
-        } catch (error) { next(error) }
-    }
+  async create(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email
+      let data = await commentsService.create(req.body)
+      return res.status(201).send(data)
+    } catch (error) { next(error) }
+  }
 
-    async edit(req, res, next) {
-        try {
-            let data = await commentsService.edit(req.params.id, req.userInfo.email, req.body)
-            return res.send(data)
-        } catch (error) { next(error) }
-    }
+  async edit(req, res, next) {
+    try {
+      let data = await commentsService.edit(req.params.id, req.userInfo.email, req.body)
+      return res.send(data)
+    } catch (error) { next(error) }
+  }
 
-    async delete(req, res, next) {
-        try {
-            await commentsService.delete(req.params.id)
-            return res.send("Successfully deleted")
-        } catch (error) { next(error) }
-    }
+  async delete(req, res, next) {
+    try {
+      await commentsService.delete(req.params.id)
+      return res.send("Successfully deleted")
+    } catch (error) { next(error) }
+  }
 }
